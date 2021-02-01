6f74a9a978562aed72ae3332a2a656e7bae1460d
Abrar051 <shahriar1410051@gmail.com>
2021-02-01 12:15:12 +0600

    Update

--- src/GenericDemo.java
+++ src/GenericDemo.java
@@ -1,6 +1,17 @@
+class Container <T>
+{
+    T value;
+    public void show ()
+    {
+        System.out.println(value.getClass().getName());
+    }
+}
 public class GenericDemo {
     public static void main (String args[])
     {
-        System.out.println();
+        //System.out.println("");
+        Container <Integer> obj = new Container<>();
+        obj.value=9;
+        obj.show();
     }
 }
